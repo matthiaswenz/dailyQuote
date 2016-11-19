23261c39dfca0deef77d0a02d415db38a1e2aba3
Robin-Manuel Thiel <robin-manuel@thiel1.de>
2016-11-19 01:36:42 +0100

    Update Xamarin.Forms packages

--- Xamarin/Forms/DailyQuote.Forms/DailyQuote.Forms/MainPage.xaml.cs
+++ Xamarin/Forms/DailyQuote.Forms/DailyQuote.Forms/MainPage.xaml.cs
@@ -1,9 +1,12 @@
-﻿using System;
+﻿using ModernHttpClient;
+using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Http;
 using System.Text;
 using System.Threading.Tasks;
 using Xamarin.Forms;
+using Newtonsoft.Json;
 
 namespace DailyQuote.Forms
 {
@@ -12,6 +15,22 @@ namespace DailyQuote.Forms
         public MainPage()
         {
             InitializeComponent();
+			QuoteLabel.GestureRecognizers.Add(new TapGestureRecognizer { Command = new Command(async () => await FetchQuoteAsync()) });
         }
+
+		protected override async void OnAppearing()
+		{
+			base.OnAppearing();
+			await FetchQuoteAsync();
+		}
+
+		private async Task FetchQuoteAsync()
+		{			
+            var httpClient = new HttpClient(new NativeMessageHandler());
+			var json = await httpClient.GetStringAsync("https://myrandomfuncs.azurewebsites.net/api/BullshitBingo?maxlength=180");
+			var quoteResult = JsonConvert.DeserializeObject<QuoteResult>(json);
+
+			QuoteLabel.Text = $"\"{quoteResult.Strings[0]}\"";
+		}
     }
 }
---
+++ Xamarin/Forms/DailyQuote.Forms/DailyQuote.Forms/Models/QuoteResult.cs
@@ -0,0 +1,10 @@
+﻿using System;
+namespace DailyQuote.Forms
+{
+	public class QuoteResult
+	{
+		public string[] Strings { get; set; }
+		public int Count { get; set; }
+		public int Total { get; set; }
+	}
+}
